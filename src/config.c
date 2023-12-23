ed53db5eac11c22a633b9db4fb7077c71f99223d
Ben Landon <81256789+https123456789@users.noreply.github.com>
2023-12-22 17:15:58 -0800

    fix: remove unneeded config

--- src/config.c
+++ src/config.c
@@ -4,16 +4,11 @@
 struct config generate_config(void) {
     struct config c;
     struct config_group_def *browser = malloc(sizeof(struct config_group_def));
-    struct config_group_def *zsh = malloc(sizeof(struct config_group_def));
 
     // Setup the browser group to select processes who's name matches 'firefox-developer-edition'
     browser->name = "browsers";
     browser->target = "/usr/lib/firefox-developer-edition/firefox";
-    browser->next = zsh;
-
-    zsh->name = "shells";
-    zsh->target = "/usr/bin/zsh";
-    zsh->next = NULL;
+    browser->next = NULL;
 
     c.groups = browser;
 
